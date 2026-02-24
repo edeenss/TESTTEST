a4b15239bdb9a69aafeaee532c63fbf03deb0e99
DESKTOP-H6GDJ27\se_im <lse223111@gmail.com>
2026-02-24 16:36:32 +0900

    add

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -1,13 +1,13 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 export default function Start() {
   return (
     <div className="page start-page">
       <h1>덕후 테스트</h1>
-      <p>총 10문제입니다. 페이징으로 한 문제씩 이동해요.</p>
+      <p>총 10문제입니다.</p>
       <Link to="/q/1" className="btn">
         시작하기
       </Link>
     </div>
-  )
+  );
 }
